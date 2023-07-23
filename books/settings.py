abf2fb29ec81ca60c6fa1f91c05f212c581f871d
Biplab Mondal <76585631+biplabmon@users.noreply.github.com>
2023-07-23 11:49:03 +0530

    Update settings.py

--- books/settings.py
+++ books/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+EXTERNAL_APPS = [
     'book_api',
     'rest_framework',
 ]
+
+INSTALLED_APPS += EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
